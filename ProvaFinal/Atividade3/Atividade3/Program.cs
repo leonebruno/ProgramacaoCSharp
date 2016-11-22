b837950824312d1fced756e664cc68e9da398d5e
Bruno Leone Alexandre Pacheco Corrêa <leone.analistadesistemas@outlook.com.br>
2016-11-22 21:11:14 -0200

    Add files via upload

---
+++ ProvaFinal/Atividade3/Atividade3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valor = new int[30];
+            int menor = 0;
+            int atual = 0;
+            int maior = 0;
+
+            for (int i = 0; i<=5; i++)
+            {
+                Console.WriteLine("Entre com o valor: ");
+                valor[i] = int.Parse(Console.ReadLine());
+
+                atual = valor[i];
+
+                if (valor[0] != 0)
+                {
+                    menor = atual;
+                }
+                if (atual < menor)
+                {
+                    menor = atual;
+                }
+                else if (atual > maior)
+                {
+                    maior = atual;
+                }
+            }
+
+            Console.WriteLine("Menor é " + menor + " e maior é " + maior);
+
+            Console.ReadKey();
+        }
+    }
+}
